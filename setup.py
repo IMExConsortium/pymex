f359848f8b5b7781486f6a6caad9bb43f27e70f1
Lukasz Salwinski <lukasz@mbi.ucla.edu>
2019-12-13 16:38:33 -0800

    nitial import.

--- setup.py
+++ setup.py
@@ -2,29 +2,30 @@ import setuptools
 import os
 from distutils.util import convert_path
 
-with open("README.md", "r") as fh:
-        long_description = fh.read()
+ldesc =[]
 
+with open( "PROJECT.md", "r") as fh:
+        long_description = fh.read()
+                
 pkg_dir = convert_path('pylib/pymex')
 version_file = open(os.path.join(pkg_dir, 'VERSION'))
 version = version_file.read().strip()
 
-setuptools.setup(name="pymex-lukasz99",
-                 packages = ['pymex'],
-                 version=version,
-                 author="Lukasz Salwinski",
-                 author_email="lukasz@mbi.ucla.edu",
-                 description="PSI-MI/IMEx Data Access",
-                 long_description=long_description,
-                 long_description_content_type="text/markdown",
-                 url="https://github.com/IMExConsortium/pymex",
-                 download_url = 'https://github.com/IMExConsortium/pymex/archive/0.0.11.tar.gz',
-                 package_dir={'': 'pylib'},
-                 include_package_data=True,
-                 packages=setuptools.find_packages(where="pylib"),
-                 classifiers=[
-                         "Programming Language :: Python :: 3",
-                         "License :: OSI Approved :: MIT License",
+setuptools.setup( name="pymex-lukasz99",
+                  version=version,
+                  author="Lukasz Salwinski",
+                  author_email="lukasz@mbi.ucla.edu",
+                  description="PSI-MI/IMEx Data Access",
+                  long_description=long_description,
+                  long_description_content_type="text/markdown",
+                  url="https://github.com/IMExConsortium/pymex",
+                  download_url = 'https://github.com/IMExConsortium/pymex/archive/0.0.11.tar.gz',
+                  package_dir={'': 'pylib'},
+                  include_package_data=True,
+                  packages=setuptools.find_packages(where="pylib"),
+                  classifiers=[
+                          "Programming Language :: Python :: 3",
+                          "License :: OSI Approved :: MIT License",
                          "Operating System :: OS Independent",
                          "Development Status :: 3 - Alpha"
                  ],
