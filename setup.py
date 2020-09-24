a530faa59c72fdde09a5f840fc7730d3bccca456
Lukasz Salwinski <lukasz@mbi.ucla.edu>
2020-09-24 16:48:47 -0700

    Beta release candidate

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup( name="pymex-lukasz99",
                   long_description=long_description,
                   long_description_content_type='text/markdown',
                   url="https://github.com/IMExConsortium/pymex",
-                  download_url = 'https://github.com/IMExConsortium/pymex/archive/0.9.3.tar.gz',
+                  download_url = 'https://github.com/IMExConsortium/pymex/archive/0.9.4.tar.gz',
                   package_dir={'': 'pylib'},                
                   package_data={
                           'pymex': ['mif/*.json', 'mif/*.jmif','mif/*.xsd'],
